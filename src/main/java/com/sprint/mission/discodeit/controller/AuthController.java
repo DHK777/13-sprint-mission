9a1511546092f91fc1cd2d5b910b9834dfb588e6
DHK777 <kjckjcdo@gmail.com>
2026-09-10 15:24:51 +0900

    feat: 로그인 인증 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,19 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.LoginRequest;
-import com.sprint.mission.discodeit.dto.UserDto;
-import com.sprint.mission.discodeit.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Auth", description = "인증 API")
 @RestController
 @RequestMapping("/api/auth")
-@RequiredArgsConstructor
 public class AuthController {
-
-  private final AuthService authService;
 
   @Operation(summary = "CSRF 토큰 발급")
   @GetMapping("/csrf-token")
@@ -36,12 +26,5 @@ public class AuthController {
     return ResponseEntity
         .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
         .build();
-  }
-
-  @Operation(summary = "로그인")
-  @PostMapping("/login")
-  public ResponseEntity<UserDto> login(@Valid @RequestBody LoginRequest request) {
-    UserDto loginUserDto = authService.login(request.username(), request.password());
-    return ResponseEntity.ok(loginUserDto);
   }
 }
